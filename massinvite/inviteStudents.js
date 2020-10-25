f97ccfb6d92d9dbd9f03bb3dc4115e269e07dd45
MukkuMayc <st063775@student.spbu.ru>
2020-10-26 02:37:33 +0300

    Edit response handling in inviteStudents

--- massinvite/inviteStudents.js
+++ massinvite/inviteStudents.js
@@ -1,21 +1,39 @@
 import fetch from "node-fetch";
 import { defaultHeaders } from "../Config.js";
 import formInvitePayload from "./invitePayload.js";
+import parseString from "set-cookie-parser";
 
 async function inviteStudents(students) {
-  const res = await fetch("https://openedu.ru/upd/spbu/student/massinvite/", {
+  return await fetch("https://openedu.ru/upd/spbu/student/massinvite/", {
     headers: {
       ...defaultHeaders,
       "Content-Type":
         "multipart/form-data; boundary=---------------------------myform",
       referer: "https://openedu.ru/upd/spbu/student/massinvite/",
     },
+    redirect: "manual",
     method: "POST",
     body: formInvitePayload(students),
   })
-    .then((res) => res.status)
-    .catch((err) => console.error("error", err));
-  return res;
+    .then((res) => {
+      let task_id = new RegExp("#[0-9]+").exec(
+        parseString(res.headers.raw()["set-cookie"])[0]?.value
+      );
+      return task_id
+        ? {
+            status: 0,
+            message: parseString(res.headers.raw()["set-cookie"])[0]?.value,
+          }
+        : { status: 1, message: "Something goes wrong, there is no task id" };
+    })
+    .catch((err) => {
+      console.error("error", err);
+      return { status: 2, message: "Some error happened", error: err };
+    });
 }
+
+inviteStudents("VgTjuhPi;GXbJskHnAf@Osuu.ru;CmKHzu;tQfNvF;;;").then((res) =>
+  console.log(res.message)
+);
 
 export default inviteStudents;
