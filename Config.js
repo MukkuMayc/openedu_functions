36bf5f2aafaf4ae0efeac8347d5315d8b0cd0ec2
MukkuMayc <st063775@student.spbu.ru>
2020-10-25 23:19:35 +0300

    Move tokens to  .env file

--- Config.js
+++ Config.js
@@ -1,8 +1,4 @@
-export const CSRFToken =
-  "XlsFCc5Ah49Ov04O3W0WGkM4SF7aSTMfpgsJKZLWQG0QkNg9SAxL50ZygqmJxTAh";
-export const sessionId = "zbvhcoy214uumag7fu9zpv2alybl0kfm";
-export const user = "fortyways";
 export const defaultHeaders = {
   "Accept-Language": "en,en-US;q=0.8,ru-RU;q=0.5,ru;q=0.3",
-  Cookie: `csrftoken=${CSRFToken}; sessionid=${sessionId}; authenticated=1; authenticated_user=${user}`,
+  Cookie: `csrftoken=${process.env.CSRF_TOKEN}; sessionid=${process.env.SESSION_ID}; authenticated=1; authenticated_user=${process.env.AUTHENTICATED_USER}`,
 };
