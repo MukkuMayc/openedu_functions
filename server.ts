445b4f098f7c84b29a0c88af4d4745725e8449a7
mukkumayc <st063775@student.spbu.ru>
2020-11-12 18:05:07 +0300

    Add file uploading

--- server.ts
+++ server.ts
@@ -2,21 +2,25 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import * as yup from "yup";
+import fs from "fs";
+import dotenv from "dotenv";
+import multer from "multer";
+import csv from "csv-parser";
 import inviteStudents from "./massinvite/inviteStudents";
 import enrollStudents from "./massenroll/enrollStudents";
 import unenrollStudents from "./massunenroll/unenrollStudents";
 import uploadCertificate from "./certificates/uploadCertificate";
-import fs from "fs";
 import { askEmail, askPassword } from "./common/askCredentials";
 import authenticate from "./authentication/authentificate";
 import getMWToken from "./authentication/getMWToken";
-import dotenv from "dotenv";
 
 const result = dotenv.config();
 
 if (result.error && !result.error.message.includes("ENOENT")) {
   throw result.error;
 }
+
+const upload = multer({ dest: "./upload" });
 
 const app = express();
 
@@ -28,69 +32,71 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
-app.get("/", (_req, res) => {
-  const resText =
-    "/invite\n" +
-    'fetch("hostname/invite", {\n' +
-    '  method: "POST"\n' +
-    "  headers: {\n" +
-    '    "Content-Type": "application/json; charset=UTF-8",\n' +
-    "  },\n" +
-    "  body: {\n" +
-    '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
-    "  }\n" +
-    "})\n" +
-    "\n" +
-    "/enroll\n" +
-    'fetch("hostname/enroll", {\n' +
-    '  method: "POST"\n' +
-    "  headers: {\n" +
-    '    "Content-Type": "application/json; charset=UTF-8",\n' +
-    "  },\n" +
-    "  body: {\n" +
-    "    courseInfo: {\n" +
-    '      tag: "edu_tech",\n' +
-    '      session: "fall_2020_spbu_spec"\n' +
-    "    }\n" +
-    '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
-    "  }\n" +
-    "})\n" +
-    "\n" +
-    "/unenroll\n" +
-    'fetch("hostname/unenroll", {\n' +
-    '  method: "POST"\n' +
-    "  headers: {\n" +
-    '    "Content-Type": "application/json; charset=UTF-8",\n' +
-    "  },\n" +
-    "  body: {\n" +
-    "    courseInfo: {\n" +
-    '      tag: "edu_tech",\n' +
-    '      session: "fall_2020_spbu_spec"\n' +
-    "    }\n" +
-    '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
-    "  }\n" +
-    "})\n" +
-    "\n" +
-    "/certificate\n" +
-    'fetch("hostname/certificate", {\n' +
-    '  method: "POST"\n' +
-    "  headers: {\n" +
-    '    "Content-Type": "application/json; charset=UTF-8",\n' +
-    "  },\n" +
-    "  body: {\n" +
-    '    email: "me@example.com"\n' +
-    "    fullName: {\n" +
-    "      name: Name,\n" +
-    "      surname: Surname,\n" +
-    "      secondName: Second,\n" +
-    "    }\n" +
-    "    grade: 95\n" +
-    '    certificateUrl: "http://www.africau.edu/images/default/sample.pdf"\n' +
-    "    courseName: 2017-006-001 Базы данных (15.02.2017 - 20.05.2017)" +
-    "  }\n" +
-    "})\n";
-  res.send(resText);
-});
+// app.get("/", (_req, res) => {
+//   const resText =
+//     "/invite\n" +
+//     'fetch("hostname/invite", {\n' +
+//     '  method: "POST"\n' +
+//     "  headers: {\n" +
+//     '    "Content-Type": "application/json; charset=UTF-8",\n' +
+//     "  },\n" +
+//     "  body: {\n" +
+//     '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
+//     "  }\n" +
+//     "})\n" +
+//     "\n" +
+//     "/enroll\n" +
+//     'fetch("hostname/enroll", {\n' +
+//     '  method: "POST"\n' +
+//     "  headers: {\n" +
+//     '    "Content-Type": "application/json; charset=UTF-8",\n' +
+//     "  },\n" +
+//     "  body: {\n" +
+//     "    courseInfo: {\n" +
+//     '      tag: "edu_tech",\n' +
+//     '      session: "fall_2020_spbu_spec"\n' +
+//     "    }\n" +
+//     '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
+//     "  }\n" +
+//     "})\n" +
+//     "\n" +
+//     "/unenroll\n" +
+//     'fetch("hostname/unenroll", {\n' +
+//     '  method: "POST"\n' +
+//     "  headers: {\n" +
+//     '    "Content-Type": "application/json; charset=UTF-8",\n' +
+//     "  },\n" +
+//     "  body: {\n" +
+//     "    courseInfo: {\n" +
+//     '      tag: "edu_tech",\n' +
+//     '      session: "fall_2020_spbu_spec"\n' +
+//     "    }\n" +
+//     '    students: "user1;mail1@ma.ru;Name;LastName;;;\\nuser2;mail2@MumX.ru;Name;LastName;;;\n"' +
+//     "  }\n" +
+//     "})\n" +
+//     "\n" +
+//     "/certificate\n" +
+//     'fetch("hostname/certificate", {\n' +
+//     '  method: "POST"\n' +
+//     "  headers: {\n" +
+//     '    "Content-Type": "application/json; charset=UTF-8",\n' +
+//     "  },\n" +
+//     "  body: {\n" +
+//     '    email: "me@example.com"\n' +
+//     "    fullName: {\n" +
+//     "      name: Name,\n" +
+//     "      surname: Surname,\n" +
+//     "      secondName: Second,\n" +
+//     "    }\n" +
+//     "    grade: 95\n" +
+//     '    certificateUrl: "http://www.africau.edu/images/default/sample.pdf"\n' +
+//     "    courseName: 2017-006-001 Базы данных (15.02.2017 - 20.05.2017)" +
+//     "  }\n" +
+//     "})\n";
+//   res.send(resText);
+// });
+
+app.use(express.static("static"));
 
 app.post("/invite", async (req, res) => {
   const yupBody = yup.object().shape({
@@ -191,6 +197,54 @@ app.post("/certificate", async (req, res) => {
         })
     )
     .catch((err) => res.status(400).send(err.toString()));
+});
+
+app.post("/file", upload.single("uploaded_file"), async (req, res) => {
+  interface CSVField {
+    Email: string;
+    Name: string;
+    Surname: string;
+    SecondName: string;
+    CourseFullName: string;
+  }
+
+  let students: CSVField[] = [];
+  await new Promise((resolve) =>
+    fs
+      .createReadStream(req.file.path)
+      .pipe(csv({ separator: ";" }))
+      .on("data", (row) => {
+        students.push(row);
+      })
+      .on("end", () => {
+        console.log("CSV file successfully processed");
+        console.log("Loaded", students.length, "users");
+        console.log("STUDENTS\n", students);
+        resolve();
+      })
+  );
+
+  const data = fs.readFileSync(req.file.path);
+  await inviteStudents(String(data)).then((res) => {
+    console.log(res);
+  });
+
+  const list = students.reduce(
+    (acc: Map<string, Array<CSVField>>, item: CSVField) => {
+      if (!acc.get(item.CourseFullName)) {
+        acc.set(item.CourseFullName, []);
+      }
+
+      acc.get(item.CourseFullName)!.push(item);
+      return acc;
+    },
+    new Map()
+  );
+
+  console.log(list);
+
+  console.log(req.file, req.body);
+  res.send("ok");
 });
 
 const port = process.env.PORT || 8080;
