e496e346b893573f74802d9d21fe8b4bb18187d5
MukkuMayc <st063775@student.spbu.ru>
2020-11-20 14:59:42 +0300

    Prettier, add StrictMode

--- static/src/App.tsx
+++ static/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Authenticate from "./components/Authenticate"
-import InviteEnroll from "./components/InviteEnroll"
+import Authenticate from "./components/Authenticate";
+import InviteEnroll from "./components/InviteEnroll";
 
 const App = () => {
   return (
@@ -10,10 +10,10 @@ const App = () => {
         <nav>
           <Link to="/">HOMO</Link> {" | "}
           <Link to="/authenticate">Authenticate</Link> {" | "}
-          <Link to="/invite-enroll">Invite and enroll students</Link> {" | "}
+          <Link to="/invite-enroll">Invite and enroll students</Link>
         </nav>
         <Switch>
-        <Route path="/authenticate">
+          <Route path="/authenticate">
             <Authenticate />
           </Route>
           <Route path="/invite-enroll">
