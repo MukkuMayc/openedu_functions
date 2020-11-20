5b06aa0804de162ac8aa80bd20779dff695d2c5a
MukkuMayc <st063775@student.spbu.ru>
2020-11-20 14:58:57 +0300

    Refactor Authenticate,  replace email with username

--- static/src/components/Authenticate.tsx
+++ static/src/components/Authenticate.tsx
@@ -4,46 +4,38 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 async function authenticate(username: string, password: string) {
   const res = await fetch("/api/authenticate", {
     method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username, password
-    })
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
   });
   return await res.text();
 }
 
-const Authenticate: React.FunctionComponent<any> = () => (
+const Authenticate = () => (
   <div>
-    <h1>Any place in your app!</h1>
+    <h1>Authorize to Openedu</h1>
     <Formik
-      initialValues={{ email: "", password: "" }}
-      validate={(values) => {
-        const errors: { email?: string; password?: string } = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
+      initialValues={{ username: "", password: "" }}
+      validate={(values) => (!values.username ? { username: "Required" } : {})}
       onSubmit={(values, { setSubmitting }) => {
         setSubmitting(true);
-        authenticate(values.email, values.password).then(res => {
+        authenticate(values.username, values.password).then((res) => {
           alert(res);
           setSubmitting(false);
-        })
+        });
       }}
     >
       {({ isSubmitting }) => (
         <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
